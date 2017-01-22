a574ba6398aea264ce4616cc582fc0920851bf7e
geographika <geographika@gmail.com>
2017-01-22 13:41:41 +0100

    Update pprint tests

--- mappyfile/pprint.py
+++ mappyfile/pprint.py
@@ -1,80 +1,4 @@
-from collections import OrderedDict
-
-# Constant used to as the dict key to store
-# the Mapfile object type e.g. MAP, LAYER, STYLE
-
-MAPFILE_TYPE = "__type__"
-
-class NonQuotedList(list):
-    """
-    Used for property lists that should be left unquoted. 
-    E.g. a POINTS block can be defined as follows:
-
-    POINTS
-        0 100
-        100 200
-        40 90
-    END
-    """
-    pass
-
-class QuotedList(list):
-    """
-    Used for property lists that should be quoted. 
-    E.g. the PROJECTION block can be defined as follows:
-
-    PROJECTION
-        'proj=utm'
-        'ellps=GRS80'
-        'datum=NAD83'
-        'zone=15'
-        'units=m'
-        'north'
-        'no_defs'
-    END
-
-    """
-    pass
-
-class QuotedString(str):
-    """
-    Used for quoted property values
-    E.g.
-
-    + NAME "Layer1"
-    + DATA "lakes.shp"
-    """
-    pass
-
-class NonQuotedString(str):
-    """
-    Used for property values which aren't quoted.
-    E.g.
-
-    + WIDTH 1
-    + COLOR 255 0 0
-    """
-    pass
-
-class HiddenContainer(list):
-    """"
-    These containers are not outputted as part of the pprint
-    They are used to store objects of the same type
-    E.g. LAYERs, CLASSes, STYLEs
-    """
-    pass
-
-class Container(OrderedDict):
-    """
-    This is for container declarations which finish with the
-    keyword END. Examples:
-    
-    + MAP ... END
-    + LAYER ... END
-    + CLASS ... END
-    + STYLE ... END
-    """
-    pass
+from mappyfile.types import *
 
 class PrettyPrinter:
     def __init__(self, indent=4, spacer=" ", quote="'", lfchar="\n"):
@@ -97,11 +21,10 @@ class PrettyPrinter:
 
         if isinstance(val, QuotedList):
             vals = map(self.quote_value, val)
-        elif isinstance(val, NonQuotedList):
-            vals = map(str, val)
         else:
-            raise ValueError("Unknown type %s for %s!" % (type(val), val))
-    
+            assert(isinstance(val, list))
+            vals = map(str, val)    
+
         s = self.lfchar.join([spacer + v for v in vals])
 
         return s
@@ -111,13 +34,34 @@ class PrettyPrinter:
         Print out a string
         """
         if isinstance(val, QuotedString):
-            v = self.quote_value(val)               
-        elif isinstance(val, NonQuotedString):
-            v = str(val)
+            val = self.quote_value(val)               
         else:
-            raise ValueError("Unknown type %s for %s!" % (type(val), val))
+            val = str(val)
 
-        return v
+        return val
+
+    def format_key(self, key):
+        """
+        All non-quoted keys should be un uppercase
+        Otherwise do not modify input
+        """
+        if isinstance(key, QuotedString):
+            key = self.format_value(key) 
+        else:
+            key = self.format_value(key.upper())
+
+        return key
+
+    def format_line(self, spacer, key, value):
+
+        tmpl = "{spacer}{key} {value}"
+        d = {
+            "spacer": spacer,
+            "key": self.format_key(key),
+            "value": self.format_value(value)
+            }
+        return tmpl.format(**d)
+
 
     def pprint(self, container):
         """
@@ -145,26 +89,32 @@ class PrettyPrinter:
                 for v in value:
                     lines += self._format(v, level + 1)
             elif isinstance(value, Container):
-                lines += self._format(value, level)
+                lines += self._format(value, level + 1)
             else:
                 if key != MAPFILE_TYPE:
                     # properties
                     spacer = self.spacer * (level + 1)
-                    ukey = str(key).upper()
 
-                    if isinstance(value, (QuotedList, NonQuotedList)):
+                    if isinstance(value, KeyValueList):
+                        for v in value:
+                            lines.append(self.format_line(spacer, key, v))
+                    elif isinstance(value, list):
+                        # add KEYWORD to signify start of block
+                        lines.append(spacer + self.format_key(key))
+
+                        # add the items in the list
                         list_spacer = self.spacer * (level + 2)
                         s = self.format_list(value, list_spacer)
-                        lines.append(spacer + ukey)
                         lines.append(s)
+
+                        # add END to close the block
                         s = self.spacer * (level + 1) + self.end
                         lines.append(s)
                     else:
-                        myval = self.format_value(value)
-                        s = spacer + ukey + " " + myval
-                        lines.append(s)
+                        lines.append(self.format_line(spacer, key, value))
 
-        if isinstance(container, Container):            
+        if isinstance(container, Container):
+            # close the container block with an END            
             s = self.spacer * level + self.end
             lines.append(s)
 
--- tests/test_pprint.py
+++ tests/test_pprint.py
@@ -1,79 +1,84 @@
-from mappyfile.pprint import QuotedList, NonQuotedList, QuotedString, NonQuotedString, Container, HiddenContainer
-from mappyfile.pprint import PrettyPrinter, MAPFILE_TYPE
-import json
+from mappyfile.pprint import PrettyPrinter
+from tests import helper
 
-def create_sample_dict():
-    style1 = Container()
-    style1[MAPFILE_TYPE] = "style"
-    style1["color"] = NonQuotedString("99 231 117")
-    style1["width"] = NonQuotedString("1")
+def export_map():
+    map_ = helper.create_sample_map()
 
-    style2 = Container()
-    style2[MAPFILE_TYPE] = "style"
-    style2["name"] = QuotedString("MyStyle")
-    style2["color"] = NonQuotedString("108 201 187")
-    style2["width"] = NonQuotedString("2")
+    pp = PrettyPrinter()
+    print(pp.pprint(map_))
 
-    styles = HiddenContainer([style1, style2])
+    # print a partial part of the map
+    print(pp.pprint(map_["layers"][0]["classes"][0]))
+    print(pp.pprint(map_["layers"][0]["classes"]))
 
-    class1 = Container()
-    class1[MAPFILE_TYPE] = "class"
-    class1["name"] = QuotedString("Class1")
-    class1["styles"] = styles
+def test_print_style():
+    style1 = create_style1()
+    pp = PrettyPrinter() # expected
+    txt = pp.pprint(style1)
+    expected = """STYLE
+    COLOR 99 231 117
+    WIDTH 1
+END"""
+    assert(txt == expected)
 
-    classes = HiddenContainer([class1])
+def test_print_map():
+    expected = """MAP
+    WEB
+        METADATA
+            'wms_enable_request' '*'
+        END
+    END
+    PROJECTION
+        'proj=utm'
+        'ellps=GRS80'
+        'datum=NAD83'
+        'zone=15'
+        'units=m'
+        'north'
+        'no_defs'
+    END
+    LAYER
+        NAME 'Layer1'
+        CLASS
+            NAME 'Class1'
+            STYLE
+                COLOR 99 231 117
+                WIDTH 1
+            END
+            STYLE
+                NAME 'MyStyle'
+                COLOR 108 201 187
+                WIDTH 2
+            END
+        END
+        FEATURE
+            POINTS
+                0 100
+                100 200
+                40 90
+            END
+        END
+    END
+    LAYER
+        NAME 'Layer2'
+        PROCESSING 'BANDS=1'
+        PROCESSING 'CONTOUR_ITEM=elevation'
+        PROCESSING 'CONTOUR_INTERVAL=20'
+    END
+END"""
 
-    points = NonQuotedList(["0 100", "100 200", "40 90"])
-    feature1 = Container()
-    feature1[MAPFILE_TYPE] = "feature"
-    feature1["points"] = points
-    features = HiddenContainer([feature1])  
+    mf = create_sample_dict()
+    pp = PrettyPrinter() # expected
+    txt = pp.pprint(mf)
+    assert(expected == txt)
 
-    layer1 = Container()
-    layer1[MAPFILE_TYPE] = "layer"
-    layer1["name"] = QuotedString("Layer1")
-    layer1["classes"] = classes
-    layer1["features"] = features
+def run_tests():
+    import pytest
+    
+    #pytest.main(["tests/test_pprint.py::test_print_map"])
+    pytest.main(["tests/test_pprint.py"])
 
-    layer2 = Container()
-    layer2[MAPFILE_TYPE] = "layer"
-    layer2["name"] = QuotedString("Layer2")
-
-    layers = HiddenContainer([layer1, layer2])
-
-    map_ = Container()
-    map_[MAPFILE_TYPE] = "map"
-
-    projection = QuotedList(["proj=utm", "ellps=GRS80", "datum=NAD83", "zone=15", "units=m", "north", "no_defs"])
-
-    map_["projection"] = projection
-    map_["layers"] = layers
-
-    return map_
-
-def print_dict_structure(d):
-    # http://stackoverflow.com/questions/3229419/pretty-printing-nested-dictionaries-in-python
-    print json.dumps(d, indent=4)
-
-def add_layer(d):
-    layer = Container()
-    layer[MAPFILE_TYPE] = "layer"
-    layer["name"] = QuotedString("Layer1.5")
-    d["layers"].insert(1, layer)
-
-map_ = create_sample_dict()
-print_dict_structure(map_)
-
-pp = PrettyPrinter()
-print(pp.pprint(map_))
-
-add_layer(map_)
-print(pp.pprint(map_))
-
-# print a partial part of the map
-print(pp.pprint(map_["layers"][0]["classes"][0]))
-print(pp.pprint(map_["layers"][0]["classes"]))
-
-
-
+if __name__ == "__main__":
+    run_tests()
+    #export_map()
 
