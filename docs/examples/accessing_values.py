5defebbdc726d350603f865342b4e0a75c94f43f
geographika <geographika@gmail.com>
2017-02-05 23:10:51 +0100

    Updated examples

--- docs/examples/accessing_values.py
+++ docs/examples/accessing_values.py
@@ -1,28 +1,17 @@
-from mappyfile.parser import Parser
+import mappyfile
 
-p = Parser()
-
-# parse will accept a filename or a string
-mapfile = p.parse_file("./docs/examples/raster.map")
+# load will accept a filename (loads will accept a string)
+mapfile = mappyfile.load("./docs/examples/raster.map")
 
 # print the map name
-print(mapfile["name"]) # would output "MyMap"
+print(mapfile["name"]) # outputs "MyMap"
        
 # access layers
 layers = mapfile["layers"]
-layer1 = layers[0] # access by index
-	
-layer2 = layers["layer2"] # access by layer NAME property
+layer2 = layers[1] # access by index
 	
 # access classes in a layer
-classes = layer1["classes"]
+classes = layer2["classes"]
 
 for c in classes:
     print(c["name"])
-
-# if the AttrDict approach is taken then the following could also be used
-# could be added for a more polished version
-    
-print(mapfile.name) 
-layer2 = layers.layer2
-print(layer2.classes[0].name)
