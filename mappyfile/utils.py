8ee7a08fb24dc3de71ec1cd69a3fe2db781b13c0
geographika <geographika@gmail.com>
2017-02-06 17:48:36 +0100

    Add new high-level functions

--- mappyfile/utils.py
+++ mappyfile/utils.py
@@ -5,9 +5,7 @@ from mappyfile.pprint import PrettyPrinter
 import os
 import codecs
 
-def load(fn):
-
-    cwd = os.path.dirname(fn)
+def load(fn, cwd=None):
 
     p = Parser(cwd=cwd)
     ast = p.parse_file(fn)
@@ -24,9 +22,15 @@ def loads(s, cwd=None):
 
     return d   
 
+def dump(d, output_file):
+
+    map_string = _pprint(d)
+    _save(output_file, map_string)
+
+    return output_file
+
 def dumps(d):
-    pp = PrettyPrinter()
-    return pp.pprint(d)
+    return _pprint(d)
 
 def find(lst, key, value):
 
@@ -45,9 +49,11 @@ def find(lst, key, value):
 def __find__(lst, key, value):
     return next((item for item in lst if item[key.lower()] == value), None)
 
-def save(map_string, output_file):
+def _save(output_file, map_string):
 
     with codecs.open(output_file, "w", encoding="utf-8") as f:
         f.write(map_string)
 
-    return output_file
+def _pprint(d):
+    pp = PrettyPrinter()
+    return pp.pprint(d)
