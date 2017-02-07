217c1289beaafc144c103a6d00cdc4cdf0a58209
geographika <geographika@gmail.com>
2017-02-07 15:36:31 +0100

    Update fields for pypi

--- setup.py
+++ setup.py
@@ -5,14 +5,18 @@ def readme():
         return f.read()
 
 setup(name='mappyfile',
-      version='0.1',
-      description='The funniest joke in the world',
+      version='0.1.0',
+      description='A pure Python MapFile parser for working with MapServer',
       long_description=readme(),
       classifiers=[
         # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Intended Audience :: Developers',
         'Topic :: Text Processing :: Linguistic',
         'Topic :: Software Development :: Build Tools'
@@ -22,4 +26,5 @@ setup(name='mappyfile',
       author_email='sethg@geographika.co.uk',
       license='MIT',
       packages=['mappyfile'],
+      install_requires=['plyplus'],
       zip_safe=False)
