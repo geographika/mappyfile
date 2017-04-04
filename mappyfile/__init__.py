31e1599f4c4dc6cdab10ce613a00e754fd104554
Julien Enselme <julien.enselme@ioda.net>
2017-04-04 17:00:28 +0200

    Add a way to find all layers matching an attribute
    - Add a findall method that returns an iterator. This method can be used
      to find all layers with an attribute that match the given value.
    - Export it in __init__.py to make it easily accesible
    - Document the querying of mapfiles.

--- mappyfile/__init__.py
+++ mappyfile/__init__.py
@@ -1,2 +1,2 @@
 # allow high-level functions to be accessed directly from the mappyfile module
-from mappyfile.utils import load, loads, find, dumps, write
+from mappyfile.utils import load, loads, find, findall, dumps, write
