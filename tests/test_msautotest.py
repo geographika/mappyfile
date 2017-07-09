e878e60d4a6eabbdc44ee4b42e8d484bcd9efb74
sethg <sethg@geographika.co.uk>
2017-07-10 00:46:23 +0200

    Reparse pretty printed files

--- tests/test_msautotest.py
+++ tests/test_msautotest.py
@@ -1,3 +1,7 @@
+"""
+Parse all the test Mapfiles in msautotests, write them to a new file,
+and then test that these also parse correctly
+"""
 import os, logging, glob, json, shutil
 
 from mappyfile.pprint import PrettyPrinter
@@ -5,37 +9,11 @@ from mappyfile.parser import Parser
 from mappyfile.transformer import MapfileToDict
 import pprint
 import mappyfile
-from subprocess import Popen, PIPE
-import tempfile
 
-def create_image_from_map(map_file, dll_location):
-
-    of = tempfile.NamedTemporaryFile(delete=False, suffix=".png", prefix="tmp_")
-    of.close()
-
-    logging.debug("Creating file %s", of.name)
-    # [SHP2IMG] -m [MAPFILE] -i png -o [RESULT]
-    params = ["shp2img","-m", map_file,"-i","png","-o", of.name]
-
-    os.environ['PATH'] = dll_location + ';' + os.environ['PATH']
-    os.environ['PROJ_LIB'] = os.path.join(dll_location, "proj\SHARE")
-
-    logging.debug(" ".join(params))
-
-    p = Popen(params, stdout=PIPE, bufsize=1)
-    with p.stdout:
-        print(map_file)
-        for line in iter(p.stdout.readline, b''):
-            print(line)
-
-    p.wait() # wait for the subprocess to exit
-
-    #os.startfile(of.name)
-
-def create_copy(msautotest_fld):
+def create_copy(msautotest_fld, msautotest_copy):
 
     # first make a backup copy of msautotest
-    msautotest_copy = os.path.join(os.path.dirname(msautotest_fld), "msautotest_mappyfile")
+
 
     if os.path.isdir(msautotest_copy):
         shutil.rmtree(msautotest_copy)
@@ -47,48 +25,58 @@ def create_copy(msautotest_fld):
 
     return msautotest_copy
 
-def main(msautotest_fld, dll_location):
+def parse_mapfile(parser, transformer, pp, fn):
+    logging.debug("Parsing %s", fn)
 
-    msautotest_copy = create_copy(msautotest_fld)
+    try:
+        ast = parser.parse_file(fn)
+        #print(ast)
+    except Exception as ex:
+        logging.warning("%s could not be successfully parsed", fn)
+        logging.exception(ex)
+        raise
+
+    try:
+        d = transformer.transform(ast)
+    except Exception as ex:
+        logging.warning("%s could not be successfully transformed", fn)
+        logging.exception(ex)
+        raise
+        
+    #map_string = pp.pprint(d)
+    #print(map_string)
+
+    return d
+
+def main(msautotest_fld, create_new_copy=True):
+
+    msautotest_copy = os.path.join(os.path.dirname(msautotest_fld), "msautotest_mappyfile")
+
+    if create_new_copy:
+        create_copy(msautotest_fld, msautotest_copy)
 
     parser = Parser()
     transformer = MapfileToDict()
     pp = PrettyPrinter()
 
-    ignore_list = ["wms_inspire_scenario1.map","wms_inspire_scenario2.map"] # these two maps aren't in utf8
-    #ignore_list = []
+    # these two maps aren't in utf8, see https://github.com/mapserver/mapserver/pull/5460
+    #ignore_list = ["wms_inspire_scenario1.map","wms_inspire_scenario2.map"] 
+    ignore_list = []
 
     mapfiles = glob.glob(msautotest_fld + '/**/*.map')
-    #mapfiles = [f for f in mapfiles if '.tmp.' not in f]
     mapfiles = [f for f in mapfiles if os.path.basename(f) not in ignore_list]
     
     for fn in mapfiles:
-        logging.debug("Parsing %s", fn)
-        ast = parser.parse_file(fn)
-        #print ast
-        #r = ast.select('composite_type *') #ast.select('attr_name')
-        #continue
 
-        try:
-            d = transformer.transform(ast)
-        except Exception as ex:
-            logging.warning("%s could not be successfully transformed", fn)
-            logging.exception(ex)
-            raise
-        
-        #dpp = pprint.PrettyPrinter()
-        #print(dpp.pprint(d))
-
-        map_string = pp.pprint(d)
-        #print(map_string)
-
+        d = parse_mapfile(parser, transformer, pp, fn)
         output_file = fn.replace(msautotest_fld, msautotest_copy)
         mf = mappyfile.utils.write(d, output_file)
-        #create_image_from_map(mf, dll_location) # fn for original map
+
+        # now try reading it again
+        d = parse_mapfile(parser, transformer, pp, output_file)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     fld = r"D:\GitHub\mapserver\msautotest"
-    dll_location = r"C:\MapServer\bin"
-    main(fld, dll_location)
+    main(fld, True)
     print("Done!")
