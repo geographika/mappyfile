fcc67c90adce1258267eb445c6bd00e1e6d74540
geographika <sethg@geographika.co.uk>
2017-07-29 15:09:56 +0200

    Update performance test

--- tests/test_large_file.py
+++ tests/test_large_file.py
@@ -8,31 +8,26 @@ from mappyfile.transformer import MapfileToDict
 import cProfile
 
 
-def output():
+def output(fn):
     """
     Parse, transform, and pretty print 
     the result
     """
     p = Parser()
     m = MapfileToDict()
-    
-    fn = r"D:\Temp\large.map"
-    with open(fn) as f:
-        s = f.read()
 
-    ast = p.parse(s)
-    ##print(ast)
+    ast = p.parse_file(fn)
+    #print(ast)
     d = m.transform(ast)
-    ##print(d)
+    #print(d)
     pp = PrettyPrinter(indent=0, newlinechar=" ", quote="'")
     pp.pprint(d)
 
 if __name__ == "__main__":
-    
+    #fn = r"D:\Temp\large_map1.txt"
+    fn = r"D:\Temp\large_map2.txt"
     pr = cProfile.Profile()
     pr.enable()
-
-    #cProfile.run('output()')
-    output()
+    output(fn)
     pr.disable()
     pr.print_stats(sort='time')
