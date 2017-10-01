730fcbf0cbe741ea08946d38e8cfb75828d86626
geographika <sethg@geographika.co.uk>
2017-10-02 00:49:52 +0200

    Add validation checks

--- tests/test_msautotest.py
+++ tests/test_msautotest.py
@@ -11,6 +11,7 @@ import json
 from mappyfile.pprint import PrettyPrinter
 from mappyfile.parser import Parser
 from mappyfile.transformer import MapfileToDict
+from mappyfile.validator import Validator
 import mappyfile
 
 
@@ -34,7 +35,6 @@ def parse_mapfile(parser, transformer, pp, fn):
 
     try:
         ast = parser.parse_file(fn)
-        # print(ast)
     except Exception as ex:
         logging.warning("%s could not be successfully parsed", fn)
         logging.exception(ex)
@@ -46,9 +46,6 @@ def parse_mapfile(parser, transformer, pp, fn):
         logging.warning("%s could not be successfully transformed", fn)
         logging.exception(ex)
         raise
-
-    # map_string = pp.pprint(d)
-    # print(map_string)
 
     return d
 
@@ -67,23 +64,43 @@ def main(msautotest_fld, create_new_copy=True):
     # these two maps aren't in utf8
     # see https://github.com/mapserver/mapserver/pull/5460
     # ignore_list = ["wms_inspire_scenario1.map","wms_inspire_scenario2.map"]
-    ignore_list = []
+
+    # transparent_layer.map has an extra END, see https://github.com/mapserver/mapserver/pull/5468
+    # polyline_no_clip.map needs symbol names in quotes
+
+    ignore_list = ["poly-label-multiline-pos-auto.map", "poly-label-pos-auto.map"]  # has attributes all on the same line
 
     mapfiles = glob.glob(msautotest_fld + '/**/*.map')
     mapfiles = [f for f in mapfiles if os.path.basename(f) not in ignore_list]
 
+    # target_map = "wms_grid_reproj_to_3857.map" # "polyline_no_clip.map"
+    # mapfiles = [f for f in mapfiles if os.path.basename(f) in (target_map)]
+
+    v = Validator()
+
     for fn in mapfiles:
 
         d = parse_mapfile(parser, transformer, pp, fn)
+        if not v.validate(d):
+            logging.warning(json.dumps(d, indent=4))
+            # raise
+
         output_file = fn.replace(msautotest_fld, msautotest_copy)
         try:
+            print(mappyfile.dumps(d))
             mappyfile.utils.write(d, output_file)
         except Exception:
-            logging.info(json.dumps(d, indent=4))
+            logging.warning(json.dumps(d, indent=4))
             logging.warning("%s could not be successfully re-written", fn)
+            raise
 
         # now try reading it again
+        print(d)
+        print(json.dumps(d, indent=4))
         d = parse_mapfile(parser, transformer, pp, output_file)
+
+        if not v.validate(d):
+            logging.warning(json.dumps(d, indent=4))
 
 
 if __name__ == '__main__':
