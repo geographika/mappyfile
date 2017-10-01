fffcba3ce9f0cbe67dfd3c92ff92601039e404a0
geographika <sethg@geographika.co.uk>
2017-10-02 00:46:01 +0200

    Fix logging and pep8

--- mappyfile/validator.py
+++ mappyfile/validator.py
@@ -23,22 +23,21 @@ class Validator(object):
         if schema_name not in self.schemas.keys():
             schemas_folder = os.path.join(os.path.dirname(__file__), "schemas")
             schema = os.path.join(schemas_folder, schema_name)
-            if not os.path.isfile(schema):  
+            if not os.path.isfile(schema):
                 raise IOError("The file %s does not exist" % schema)
 
             # need to set a full file URI to the base schema
-            root_schema_path = "file:///{}".format(os.path.abspath(schema)
-                                              ).replace("\\", "/")
+            root_schema_path = "file:///{}".format(os.path.abspath(schema)).replace("\\", "/")
 
             with open(schema) as f:
                 try:
                     jsn_schema = json.loads(f.read())
                 except ValueError as ex:
-                    logging.error("Could not load %s", schema)
+                    log.error("Could not load %s", schema)
                     raise ex
 
             resolver = jsonschema.RefResolver(root_schema_path, None)
-            # cache the schema for future use     
+            # cache the schema for future use
             self.schemas[schema_name] = (jsn_schema, resolver)
         else:
             jsn_schema, resolver = self.schemas[schema_name]
@@ -47,20 +46,19 @@ class Validator(object):
 
     def convert_lowercase(self, x):
 
-      if isinstance(x, list):
-        return [self.convert_lowercase(v) for v in x]
-      elif isinstance(x, dict):
-        return OrderedDict((k.lower(), self.convert_lowercase(v)) for k, v in x.iteritems())
-      else:
-        if isinstance(x, (str, unicode)):
-            x = x.lower()
+        if isinstance(x, list):
+            return [self.convert_lowercase(v) for v in x]
+        elif isinstance(x, dict):
+            return OrderedDict((k.lower(), self.convert_lowercase(v)) for k, v in x.iteritems())
+        else:
+            if isinstance(x, (str, unicode)):
+                x = x.lower()
 
         return x
 
     def validate(self, d, schema_name="map"):
-        
+
         jsn_schema, resolver = self.get_schema(schema_name)
-                    
         lowercase_dict = self.convert_lowercase(d)
 
         jsn = json.loads(json.dumps(lowercase_dict), object_pairs_hook=OrderedDict)
