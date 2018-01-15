6a83666be1972f57d7071d36bfb49ead31ad34e3
geographika <geographika@gmail.com>
2018-01-15 09:58:18 -0800

    Initial linter module

---
+++ mappyfile/linter.py
@@ -0,0 +1,12 @@
+"""
+Design to be based on https://github.com/PyCQA/pylint/blob/master/pylint/lint.py
+
+Return text based output
+Also allow inlined comments
+
+See http://python-jsonschema.readthedocs.io/en/latest/errors/ for error properties
+"""
+
+
+def report_error():
+    pass
