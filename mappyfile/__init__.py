c4bcbbfa5f1dd72a621c3ccb38d6a01da1735015
geographika <sethg@geographika.co.uk>
2018-02-07 00:15:14 +0100

    Add plugin setup

--- mappyfile/__init__.py
+++ mappyfile/__init__.py
@@ -1,8 +1,18 @@
 import logging
+import pkg_resources
+import sys
+from types import ModuleType
+
+plugins = ModuleType('mappyfile.plugins')
+sys.modules['mappyfile.plugins'] = plugins
+
+for ep in pkg_resources.iter_entry_points(group='mappyfile.plugins'):
+    setattr(plugins, ep.name, ep.load())
+
 # allow high-level functions to be accessed directly from the mappyfile module
 from mappyfile.utils import load, loads, find, findall, dumps, write
 
-__version__ = "0.6.0"
+__version__ = "0.6.3"
 
 __all__ = ['load', 'loads', 'find', 'findall', 'dumps', 'write']
 
