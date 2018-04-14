d9a7c7dedea808d3aa4be5fd0472668e37033180
geographika <sethg@geographika.co.uk>
2018-04-14 17:53:13 +0200

    Add SYMBOLSET tests

---
+++ tests/test_symbolset.py
@@ -0,0 +1,86 @@
+import logging
+import json
+import pytest
+from mappyfile.parser import Parser
+from mappyfile.pprint import PrettyPrinter
+from mappyfile.transformer import MapfileToDict
+from mappyfile.validator import Validator
+
+
+def output(s, include_position=True, schema_name="map"):
+    """
+    Parse, transform, validate, and pretty print
+    the result
+    """
+    p = Parser()
+    m = MapfileToDict(include_position=include_position)
+    ast = p.parse(s)
+    logging.debug(ast.pretty())
+    d = m.transform(ast)
+    logging.debug(json.dumps(d, indent=4))
+    v = Validator()
+    errors = v.validate(d, schema_name=schema_name)
+    logging.error(errors)
+    pp = PrettyPrinter(indent=0, newlinechar=" ", quote="'")
+    s = pp.pprint(d)
+    logging.debug(s)
+    assert(len(errors) == 0)
+    return s
+
+
+def test_symbolset_include():
+    s = u"""
+    MAP
+        NAME "Test"
+        SYMBOLSET "./symbolset.txt"
+        SIZE 200 200
+    END
+    """
+
+    print(output(s, schema_name="map"))
+    exp = "MAP NAME 'Test' SYMBOLSET './symbolset.txt' SIZE 200 200 END"
+    assert(output(s, schema_name="map") == exp)
+
+
+@pytest.mark.xfail
+def test_symbolset_file():
+    s = u"""
+    SYMBOLSET
+        SYMBOL
+            NAME 'default-circle'
+            TYPE ELLIPSE
+            FILLED TRUE
+            POINTS
+                1 1
+            END
+        END
+        SYMBOL
+            NAME 'other-circle'
+            TYPE ELLIPSE
+            FILLED FALSE
+            POINTS
+                1 1
+            END
+        END
+    END
+    """
+
+    print(output(s, schema_name="symbolset"))
+    exp = "SYMBOLSET SYMBOL NAME 'default-circle' TYPE ELLIPSE FILLED TRUE POINTS 1 1 END " \
+          "END SYMBOL NAME 'other-circle' TYPE ELLIPSE FILLED FALSE POINTS 1 1 END END END"
+    assert(output(s, schema_name="symbolset") == exp)
+
+
+def run_tests():
+    """
+    Need to comment out the following line in C:\VirtualEnvs\mappyfile\Lib\site-packages\pep8.py
+    #stdin_get_value = sys.stdin.read
+    Or get AttributeError: '_ReplInput' object has no attribute 'read'
+    """
+    pytest.main(["tests/test_symbolset.py"])
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG)
+    run_tests()
+    print("Done!")
