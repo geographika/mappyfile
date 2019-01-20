26c5ccfeee94de097ff5f2f425a89f64bef2f159
geographika <geographika@gmail.com>
2019-01-20 10:51:12 +0100

    Fix typo

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -65,7 +65,7 @@ def test_dump_with_end_comments():
     s = '''MAP NAME "TEST" END'''
 
     d = mappyfile.loads(s)
-    output = mappyfile.dumps(d, indent=1, spacer="\t", newlinechar=" ", end_comments=True)
+    output = mappyfile.dumps(d, indent=1, spacer="\t", newlinechar=" ", end_comment=True)
     print(output)
     assert output == 'MAP 	NAME "TEST" END # MAP'
 
