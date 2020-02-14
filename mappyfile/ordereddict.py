905e9e04b54c8c4064f162fa577c6358cd1e86bd
sethg <sethg@geographika.co.uk>
2020-02-14 23:16:56 +0100

    Add license headers

--- mappyfile/ordereddict.py
+++ mappyfile/ordereddict.py
@@ -1,3 +1,37 @@
+# =================================================================
+#
+# Authors: Seth Girvin
+#          Jason Kirtland [1]
+#          Zach Kelling [2]
+#
+# [1] http://code.activestate.com/recipes/523034-emulate-collectionsdefaultdict/
+# [2] http://stackoverflow.com/a/6190500/562769
+#
+# Copyright (c) 2020 Seth Girvin
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# =================================================================
+
 from collections import OrderedDict
 import copy
 import sys
@@ -18,7 +52,7 @@ class DefaultOrderedDict(OrderedDict):
     """
     Used for storing components
     Source: http://stackoverflow.com/a/6190500/562769
-    http://code.activestate.com/recipes/523034-emulate-collectionsdefaultdict/
+    Based on: http://code.activestate.com/recipes/523034-emulate-collectionsdefaultdict/
     """
 
     def __init__(self, default_factory=None, *a, **kw):
