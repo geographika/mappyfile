d951059821374ae5def54c7494eaacf05c083128
geographika <geographika@gmail.com>
2020-07-13 01:26:51 +0200

    Minor schema fixes

--- tests/test_snippets.py
+++ tests/test_snippets.py
@@ -958,6 +958,6 @@ def run_tests():
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
-    test_polaroffset()
+    test_font_symbol()
     # run_tests()
     print("Done!")
