0aa481848bdb92ae15a723e5cfa8eec4b5442810
sethg <sethg@geographika.co.uk>
2020-07-14 07:46:30 +0200

    Run cli scripts as part of CI and add py38

--- setup.py
+++ setup.py
@@ -23,6 +23,8 @@ setup(name='mappyfile',
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
           'Intended Audience :: Developers',
           'Topic :: Text Processing :: Linguistic',
           'Topic :: Software Development :: Build Tools'
