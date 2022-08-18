e62754ecb542fda19a3e32bfa9d0f3855567d75f
sethg <sethg@geographika.co.uk>
2022-08-18 17:29:51 +0200

    Add requirement

--- setup.py
+++ setup.py
@@ -40,6 +40,7 @@ setup(name='mappyfile',
       packages=['mappyfile'],
       install_requires=[
             'lark-parser>=0.11.3',
+            'lark_cython',
             # pyrsistent is a dependency of jsonschema but py2 is not
             # supported beyond 0.16.0
             'pyrsistent<0.17.0; python_version=="2.7"',
