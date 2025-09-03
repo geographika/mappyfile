46d52672d783729645aa88aa5b0b2d89dec910f1
geographika <sethg@geographika.co.uk>
2025-09-03 09:22:46 +0200

    Draft CONFIG parser

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,7 +1,8 @@
 import pytest
 from lark import UnexpectedCharacters, UnexpectedToken, Tree
 from mappyfile.parser import Parser
-
+from mappyfile.transformer import ConfigfileTransformer, MapfileToDict
+from mappyfile.pprint import PrettyPrinter
 
 def test_parser_validation():
     p = Parser()
@@ -11,12 +12,26 @@ def test_parser_validation():
             MS_MAP_PATTERN "."
             PROJ_LIB "C:/MapServer/bin/proj7/SHARE"
     END
+    MAPS
+            test1 "C:/Maps/test1.map"
+            test2 "C:/Maps/test2.map"
+    END    
     END
     """
     tree: Tree = p.parse(config_text_ok)
     assert tree.data == "config"
     assert tree.children[0].data == "env"
 
+    m = MapfileToDict(
+        include_position=True, include_comments=True, transformer_class=ConfigfileTransformer
+    )
+    d = m.transform(tree)
+
+    pp = PrettyPrinter(indent=0, newlinechar=" ", quote="'")
+    # pp = PrettyPrinter()
+    s = pp.pprint(d)
+    print(s)    
+        
     # Test bad punctuation
     config_text_bad1 = """CONFIG
     ENV
@@ -26,7 +41,7 @@ def test_parser_validation():
     END
     """
     with pytest.raises(UnexpectedCharacters) as e:
-        p.parse(config_text_bad1)
+        res = p.parse(config_text_bad1)
     assert e.value.line == 4
 
     # Test mapfile composites inside the config
@@ -38,5 +53,14 @@ def test_parser_validation():
     END
     END
     """
-    with pytest.raises(UnexpectedToken):
-        p.parse(config_text_bad2)
+    with pytest.raises(UnexpectedToken) as e:
+        res = p.parse(config_text_bad2)
+
+def run_tests():
+    pytest.main(["tests/test_config.py"])
+
+
+if __name__ == "__main__":
+    # logging.basicConfig(level=logging.DEBUG)
+    run_tests()
+    print("Done!")
